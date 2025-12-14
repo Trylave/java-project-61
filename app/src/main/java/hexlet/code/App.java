1a0cb8dff398d7189a56fb9dd6cc1a3cd141e8e2
Trylave <yaroslav-nevskii@mail.ru>
2025-12-14 14:58:13 +0300

    add Task 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Welcome to the Brain Games!");
+        Cli.greetUser();
 	
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,21 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetUser() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (scanner.hasNextLine()) {
+                String name = scanner.nextLine();
+                System.out.println("Hello, " + name + "!");
+            } else {
+                System.out.println("Hello!");
+            }
+        } catch (Exception e) {
+            System.out.println("Hello, Guest!");
+        }
+    }
+}

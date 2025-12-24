ae505558a55b9e4404e3eb711e5995835fdb6666
Trylave <yaroslav-nevskii@mail.ru>
2025-12-24 20:36:00 +0300

    add b.g.kts config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.51.0"
     id("org.sonarqube") version "7.2.2.6593"
+    id("checkstyle")
 }
 
 group = "hexlet.code"
@@ -34,4 +35,26 @@ sonar {
     property("sonar.projectKey", "Trylave_java-project-61")
     property("sonar.organization", "trylave")
   }
+}
+checkstyle {
+    toolVersion = "10.12.5"
+    
+    // В Kotlin DSL используется isIgnoreFailures вместо ignoreFailures
+    isIgnoreFailures = false  // ← ИСПРАВЛЕНО!
+    
+    // Другие настройки
+    maxErrors = 0
+    maxWarnings = 0
+}
+
+// Альтернативный вариант конфигурации
+tasks.withType<Checkstyle>().configureEach {
+    // Используем setIgnoreFailures() метод
+    setIgnoreFailures(false)
+    
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        html.outputLocation.set(file("${buildDir}/reports/checkstyle/${name}.html"))
+    }
 }
